d3651db46a91178d8da2f9ff0ed4eb07d38177f0
Manjula <manjulamanjula0232@gmail.com>
2025-12-24 16:53:27 +0530

    Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,27 @@
 //  Created by uday on 24/12/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of the changes
+ 
+ RELEASE:
+ [Release] Description of the release
+ 
+ */
+
+
 import SwiftUI
 
 struct ContentView: View {
