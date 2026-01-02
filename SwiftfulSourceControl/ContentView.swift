2f69f217ca7ae5693acb15fc668777b6cb6fb7e1
Manjula <manjulamanjula0232@gmail.com>
2026-01-02 14:30:30 +0530

    [feature] update title name

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
                         Image(systemName: "magnifyingglass")
                             .font(.largeTitle)
                             .foregroundStyle(.tint)
-                        Text("Some new Title!")
+                        Text("Manjula Uday!!!!!")
                         
                         Button("Click me") {
                             
