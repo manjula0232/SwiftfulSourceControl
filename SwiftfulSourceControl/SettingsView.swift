943d4dc4c14e96ebd5534a823dea5b85fb733aa4
Manjula <manjulamanjula0232@gmail.com>
2026-01-05 16:46:15 +0530

    [Feature] changed the settings view

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    @State private var showSettings: Bool = false
+    @State private var showSettings: Bool = true
     
     var body: some View {
         Text("Settings!!")
