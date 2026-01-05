eed22e73445acc98f1869b0bfc8a9a18a5ab5bc1
Manjula <manjulamanjula0232@gmail.com>
2026-01-05 16:03:41 +0530

    [Relese] Fix issue

--- SwiftfulSourceControl/SettingsView.swift
+++ SwiftfulSourceControl/SettingsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     
-    @State private var showSettings: Bool = true
+    @State private var showSettings: Bool = false
     
     var body: some View {
         Text("Settings!!")
