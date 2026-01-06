7ce58228ed5d45670a76630d34429360507d2b63
Manjula <manjulamanjula0232@gmail.com>
2026-01-06 13:25:18 +0530

    [Feature] Update ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by uday on 06/01/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Manju")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
